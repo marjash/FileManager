60a71ddcf8ea478918d26d9ed76911c7fa033a7a
Oksana <marjash@i.ua>
2022-09-19 22:10:13 +0300

    add add text in specific position

--- src/main/java/command/Add.java
+++ src/main/java/command/Add.java
@@ -2,6 +2,7 @@ package command;
 
 import lombok.SneakyThrows;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -15,41 +16,45 @@ public class Add extends Command {
     @SneakyThrows
     @Override
     public String execute(List<String> args) {
-        if (args.isEmpty()) {
+        File result = null;
+        if (args.isEmpty())
             return "Incorrect argument. Use `open <file name> <text>` to open the file";
-        } else {
+        else {
             File file = context.getCurrentDirectory();
             File[] allFiles = file.listFiles();
             if (allFiles != null) {
                 for (File f : allFiles) {
-                    if (args.size() == 2) {
-                        if (f.getName().equals(args.get(0))) {
-                            FileUtils.writeStringToFile(
-                                    f, "\r\n" + args.get(1), StandardCharsets.UTF_8, true);
-                            return f.toString();
-
-                        }
-                    }
-                    if (args.size() == 3) {
-                        if (f.getName().equals(args.get(0))) {
-                            int position = Integer.parseInt(args.get(2));
-
-//                            FileUtils.writeByteArrayToFile(f, bytes, position, bytes.length - position, true);
-                            RandomAccessFile rf = new RandomAccessFile(f.getPath(), "rw");
-                            rf.seek(position);
-//                            byte[] b = new byte[(int) (rf.length() - position)];
-//                            for (int i = 0; i < b.length; i++) {
-//                                b[i] = rf.readByte();
-//                            }
-                            byte[] bytes = args.get(1).getBytes(StandardCharsets.UTF_8);
-//                            byte[] res = ArrayUtils.addAll(b, bytes);
-                            rf.write(bytes);
-                            rf.close();
-                        }
+                    if (f.getName().equals(args.get(0))) {
+                        result = f;
+                        if (args.size() == 2)
+                            addTextToEnd(args, f);
+                        if (args.size() == 3)
+                            addTextToRandomPosition(args, f);
                     }
                 }
             }
-            return null;
         }
+        return FileUtils.readFileToString(result, StandardCharsets.UTF_8);
+    }
+
+    private void addTextToEnd(List<String> args, File f) throws IOException {
+        FileUtils.writeStringToFile(
+                f, "\r\n" + args.get(1), StandardCharsets.UTF_8, true);
+    }
+
+
+    private void addTextToRandomPosition(List<String> args, File f) throws IOException {
+        int position = Integer.parseInt(args.get(2));
+        RandomAccessFile rf = new RandomAccessFile(f.getPath(), "rw");
+        byte[] textToAdd = args.get(1).getBytes(StandardCharsets.UTF_8);
+        rf.seek(position);
+        byte[] text = new byte[(int) (rf.length() - position)];
+        rf.readFully(text);
+        byte[] res = ArrayUtils.addAll(textToAdd, text);
+        rf = new RandomAccessFile(f.getPath(), "rw");
+        rf.seek(position);
+        rf.write(res);
+        rf.close();
     }
 }
+
