f76beab8f66887815cbc6c7e011f7245864b1474
Oksana <marjash@i.ua>
2022-09-19 22:39:40 +0300

    add refactoring

--- src/main/java/command/Rmdir.java
+++ src/main/java/command/Rmdir.java
@@ -22,26 +22,33 @@ public class Rmdir extends Command {
             if (files != null) {
                 for (File f : files) {
                     if (f.isDirectory()) {
-                        if (args.size() == 1 && f.getName().equals(args.get(0))) {
-                            if (FileUtils.isEmptyDirectory(f)) {
-                                FileUtils.deleteDirectory(new File(f.getName()));
-                                return f.getName() + " was deleted";
-                            } else
-                                return "failed to remove " + f.getName() + ": Directory not empty";
-                        }
-                        if (args.size() == 2) {
-                            if (f.getName().equals(args.get(1))) {
-                                if (args.get(0).equals("-r")) {
-                                    FileUtils.deleteDirectory(new File(f.getName()));
-                                    return f.getName() + " was deleted";
-                                } else
-                                    return "failed to remove " + f.getName() + ": Incorrect options";
-                            }
-                        }
+                        if (args.size() == 1 && f.getName().equals(args.get(0)))
+                            return removeEmptyDir(f);
+                        if (args.size() == 2 && f.getName().equals(args.get(1)))
+                            return RemoveDirWithFiles(args, f);
                     }
                 }
             }
         }
-        return null;
+        return "failed to remove. Is not a directory";
+    }
+
+
+    @SneakyThrows
+    private String removeEmptyDir(File f) {
+        if (FileUtils.isEmptyDirectory(f)) {
+            FileUtils.deleteDirectory(new File(f.getName()));
+            return f.getName() + " was deleted";
+        }
+        return "failed to remove " + f.getName() + ": Directory not empty";
+    }
+
+    @SneakyThrows
+    private String RemoveDirWithFiles(List<String> args, File f){
+        if (args.get(0).equals("-r")) {
+            FileUtils.deleteDirectory(new File(f.getName()));
+            return f.getName() + " was deleted";
+        }
+        return "failed to remove " + f.getName() + ": Incorrect options";
     }
 }
